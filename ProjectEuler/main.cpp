318841a597a67a433371e58a2b0611d0a2c33b2f
Christopher Atkinson <c_atkinson@outlook.com>
2018-07-11 23:05:16 +0100

    Added time calculation to templated function to see elapsed time of each solution.

--- ProjectEuler/main.cpp
+++ ProjectEuler/main.cpp
@@ -18,5 +18,9 @@ int main(int argc, char *argv[])
 template<typename T, typename... TArgs>
 inline static void Run(TArgs&&... args) 
 {
+	auto t1 = std::chrono::high_resolution_clock::now();
+
 	auto result = std::unique_ptr<T>(new T(std::forward<TArgs>(args)...)); 
+
+	std::cout << "Elapsed Time : " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t1).count() << "ms" << std::endl;
 };
